c13f37acd4c1b287d44478b76e9d1581c6fd5944
scalamoondis <schwitallabusiness@gmail.com>
2023-10-04 15:34:02 -0400

    Decided to move away from a universal button function, so the project was more flayed out and easier to read. Finished all button display functions except for equals and decimal. Beginning calculation functions and added appropriate event listeners.

--- scripting.js
+++ scripting.js
@@ -1,9 +1,10 @@
 
-/* Setting variable for ground state of calculator */
+/* Setting variable for ground state of calculator, starts in an active state because there are numbers in it */
 let currentState = 1;
 
 
-
+/* Setting initial value for calculations */
+let calculation = 0;
 
 
 
@@ -40,6 +41,18 @@ floorDivisionButton.addEventListener("click", floorDivisionButtonfunction);
 regularDivisionButton.addEventListener("click", regularDivisionButtonfunction);
 numberSevenButton.addEventListener("click", numberSevenButtonfunction);
 numberEightButton.addEventListener("click", numberEightButtonfunction);
+numberNineButton.addEventListener("click", numberNineButtonfunction);
+additionButton.addEventListener("click", additionButtonfunction);
+numberFourButton.addEventListener("click", numberFourButtonfunction);
+numberFiveButton.addEventListener("click", numberFiveButtonfunction);
+numberSixButton.addEventListener("click", numberSixButtonfunction);
+subtractionButton.addEventListener("click", subtractionButtonfunction);
+numberOneButton.addEventListener("click", numberOneButtonfunction);
+numberTwoButton.addEventListener("click", numberTwoButtonfunction);
+numberThreeButton.addEventListener("click", numberThreeButtonfunction);
+multiplicationButton.addEventListener("click", multiplicationButtonfunction);
+numberZeroButton.addEventListener("click", numberZeroButtonfunction);
+
 
 
 
@@ -59,8 +72,7 @@ function inputLimitCheck () {
 };
 
 
-/* Button functions */
-
+/* All button display functions */
 
 function ACbuttonfunction () {
 
@@ -131,6 +143,7 @@ function numberSevenButtonfunction () {
 
 };
 
+
 function numberEightButtonfunction () {
 
     inputLimitCheck();
@@ -148,14 +161,240 @@ function numberEightButtonfunction () {
 
 
 
-
-
-
-function buttonInputFunction () {
+function numberNineButtonfunction () {
 
     inputLimitCheck();
     let inputBox = document.getElementById("userInput");
-    
+    if(currentState == 0) {
+        document.getElementById("userInput").textContent = "9";
+        currentState = 1;
+
+    }else{
+        let text = document.createTextNode("9");
+        inputBox.appendChild(text);
+    };
+
+};
+
+
+function additionButtonfunction () {
+
+    inputLimitCheck();
+    let inputBox = document.getElementById("userInput");
+    if(currentState == 0) {
+        alert("Input a number first!");
+
+    }else{
+        let text = document.createTextNode("+");
+        inputBox.appendChild(text);
+
+    };
+
+};
+
+
+function numberFourButtonfunction () {
+
+    inputLimitCheck();
+    let inputBox = document.getElementById("userInput");
+    if(currentState == 0) {
+        document.getElementById("userInput").textContent = "4";
+        currentState = 1;
+
+    }else{
+        let text = document.createTextNode("4");
+        inputBox.appendChild(text);
+    };
+
+};
+
+
+function numberFiveButtonfunction () {
+
+    inputLimitCheck();
+    let inputBox = document.getElementById("userInput");
+    if(currentState == 0) {
+        document.getElementById("userInput").textContent = "5";
+        currentState = 1;
+
+    }else{
+        let text = document.createTextNode("5");
+        inputBox.appendChild(text);
+    };
+
+};
+
+
+function numberSixButtonfunction () {
+
+    inputLimitCheck();
+    let inputBox = document.getElementById("userInput");
+    if(currentState == 0) {
+        document.getElementById("userInput").textContent = "6";
+        currentState = 1;
+
+    }else{
+        let text = document.createTextNode("6");
+        inputBox.appendChild(text);
+    };
+
+};
+
+
+function subtractionButtonfunction () {
+
+    inputLimitCheck();
+    let inputBox = document.getElementById("userInput");
+    if(currentState == 0) {
+        alert("Input a number first!");
+
+    }else{
+        let text = document.createTextNode("-");
+        inputBox.appendChild(text);
+
+    };
+
+};
+
+
+function numberOneButtonfunction () {
+
+    inputLimitCheck();
+    let inputBox = document.getElementById("userInput");
+    if(currentState == 0) {
+        document.getElementById("userInput").textContent = "1";
+        currentState = 1;
+
+    }else{
+        let text = document.createTextNode("1");
+        inputBox.appendChild(text);
+    };
+
+};
+
+
+function numberTwoButtonfunction () {
+
+    inputLimitCheck();
+    let inputBox = document.getElementById("userInput");
+    if(currentState == 0) {
+        document.getElementById("userInput").textContent = "2";
+        currentState = 1;
+
+    }else{
+        let text = document.createTextNode("2");
+        inputBox.appendChild(text);
+    };
+
+};
+
+
+function numberThreeButtonfunction () {
+
+    inputLimitCheck();
+    let inputBox = document.getElementById("userInput");
+    if(currentState == 0) {
+        document.getElementById("userInput").textContent = "3";
+        currentState = 1;
+
+    }else{
+        let text = document.createTextNode("3");
+        inputBox.appendChild(text);
+    };
+
+};
+
+
+function multiplicationButtonfunction () {
+
+    inputLimitCheck();
+    let inputBox = document.getElementById("userInput");
+    if(currentState == 0) {
+        alert("Input a number first!");
+
+    }else{
+        let text = document.createTextNode("*");
+        inputBox.appendChild(text);
+
+    };
+
+};
+
+
+function numberZeroButtonfunction () {
+
+    inputLimitCheck();
+    let inputBox = document.getElementById("userInput");
+    if(currentState == 0) {
+        document.getElementById("userInput").textContent = "0";
+        currentState = 1;
+
+    }else{
+        let text = document.createTextNode("0");
+        inputBox.appendChild(text);
+    };
+
+};
+
+
+/* This is where the "=" and decimal functions will go */
+
+
+function equalsButtonfunction () {
+
+};
+
+
+function decimalButtonfunction () {
+
+};
+
+
+
+
+
+/* Operation functions */
+
+function additionOperationfunction (firstNumber, secondNumber) {
+
+    return firstNumber+secondNumber;
+
+};
+
+
+function subtractionOperationfunction (firstNumber, secondNumber) {
+
+    return firstNumber-secondNumber;
+
+};
+
+
+function multiplicationOperationfunction (firstNumber, secondNumber) {
+
+    return firstNumber*secondNumber;
+
+};
+
+
+function divisionOperationfunction (firstNumber, secondNumber) {
+
+    return firstNumber/secondNumber;
+
+};
+
+function floorDivisionOperationfunction (firstNumber, secondNumber) {
+
+    return firstNumber%secondNumber;
+
+};
+
+
+/* Calculation functions */
+
+
+function calculationFunction () {
+
+
 
 
 
